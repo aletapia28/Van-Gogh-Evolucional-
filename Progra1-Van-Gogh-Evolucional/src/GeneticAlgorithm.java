150a2540e6a949406662bcb03acb75119a691db5
Alejandro <aletapiab28@gmail.com>
2018-09-05 23:11:18 -0600

    ;

--- Progra1-Van-Gogh-Evolucional/src/GeneticAlgorithm.java
+++ Progra1-Van-Gogh-Evolucional/src/GeneticAlgorithm.java
@@ -9,6 +9,7 @@ import static java.awt.image.ImageObserver.WIDTH;
 import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
+import static java.time.Clock.system;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
 import java.util.Random;
@@ -36,13 +37,16 @@ public class GeneticAlgorithm {
     
     private static int[][] convertTo2D(BufferedImage image){
         final byte[] pixels = ((DataBufferByte) image.getRaster().getDataBuffer()).getData();
-        final int width = image.getWidth();
-        final int height = image.getHeight();
+
+       final int width = 128;//image.getWidth();
+       final int height = 128;//image.getHeight();
+
         int[][] result = new int[height][width];
         if (image.getAlphaRaster() != null) {
             final int pixelLength = 3;
             for (int pixel = 0, row = 0, col = 0; pixel < pixels.length; pixel += pixelLength) {
                 int argb = 0;
+ 
 		argb += ((int) pixels[pixel + 1] & 0xff);			   // blue
 		argb += (((int) pixels[pixel + 2] & 0xff) << 8);		// green
 		argb += (((int) pixels[pixel + 3] & 0xff) << 16);	   // red
@@ -60,7 +64,7 @@ public class GeneticAlgorithm {
                 int argb = 0;
                 argb += ((int) pixels[pixel] & 0xff);				   // blue
                 argb += (((int) pixels[pixel + 1] & 0xff) << 8);		// green
-                argb += (((int) pixels[pixel + 2] & 0xff) << 16);	   // red
+               argb += (((int) pixels[pixel + 2] & 0xff) << 16);	   // red
                 result[row][col] = argb;
                 col++;
                 if (col == width) {
@@ -273,7 +277,7 @@ public class GeneticAlgorithm {
     }    
     
     public static void main(String[] args) throws IOException {
-  	BufferedImage image = ImageIO.read(GeneticAlgorithm.class.getResource("images1.jpg"));	//read the image into the image object		
+  	BufferedImage image = ImageIO.read(GeneticAlgorithm.class.getResource("imagen32.jpg"));	//read the image into the image object		
         ImageIcon im = new ImageIcon(image);
         ventana.setVisible(true);
         ventana.jLabel2.setIcon(im);
